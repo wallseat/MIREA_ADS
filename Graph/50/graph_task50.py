2649054abdffc2c563b9331f36c3fec3e4b46549
wallseat <wallseat@gmail.com>
2021-12-28 18:38:20 +0300

    * Добавлены 50 и 85 для графов

---
+++ Graph/50/graph_task50.py
@@ -0,0 +1,53 @@
+def read_inc_matrix(filename):
+    with open(filename, 'r') as f:
+        dim = int(f.readline().strip())
+        matrix = [[0] * dim for _ in range(dim)]
+
+        inc_matrix = []
+        for line in f.readlines():
+            inc_matrix.append(list(map(int, line.split())))
+
+        for i in range(len(inc_matrix[0])):
+            first = None
+            second = None
+
+            for j in range(dim):
+                if inc_matrix[j][i] != 0:
+                    if inc_matrix[j][i] < 0:
+                        second = j
+                    else:
+                        first = j
+
+                    if first and second:
+                        break
+
+            matrix[first][second] = 1
+
+    return matrix
+
+
+def graph_root(matrix):
+    vertices: set
+    
+    def _dfs(v1: int):
+        vertices.discard(v1)
+        for v2, edge in enumerate(matrix[v1]):
+            if edge == 1:
+                _dfs(v2)
+
+        return vertices
+        
+    for i in range(len(matrix)):
+        vertices = set(j for j in range(len(matrix)))
+        if len(_dfs(i)) == 0:
+            return i
+        
+    return -1
+
+matrix = read_inc_matrix('graph_task50.txt')
+graph_root = graph_root(matrix)
+if graph_root >= 0:
+    print(f"Корень графа существует и является {graph_root}")
+else:
+    print("Корня данного графа не существует!")
+ 
---
+++ Graph/85/graph_task85.py
@@ -0,0 +1,40 @@
+import copy, random
+from math import pow
+
+def contract(vertices, edges):
+    while len(vertices) > 2:
+        ind = random.randrange(0, len(edges))
+        u, v = edges.pop(ind)
+        vertices.remove(v)
+        new_edges = []
+        for i in range(len(edges)):
+            if edges[i][0] == v:
+                edges[i][0] = u
+            elif edges[i][1] == v:
+                edges[i][1] = u
+            if edges[i][0] != edges[i][1]:
+                new_edges.append(edges[i])
+        edges = new_edges
+                
+    return len(edges)
+
+def prepare(filename: str):
+    with open(filename, 'r') as f:
+        dim = int(f.readline().strip())
+        vertices = [i for i in range(dim)]
+        edges = [list(map(int, line.split())) for line in f.readlines()]
+    
+    return vertices, edges
+
+if __name__ == '__main__':
+    result = []
+    
+    vertices, edges = prepare('graph_task85.txt')
+
+    for i in range(int(pow(len(vertices), 2))):
+        v = copy.deepcopy(vertices)
+        e = copy.deepcopy(edges)
+        r = contract(v, e)
+        result.append(r)
+
+    print("Минимальный разрез данной сети равен:", min(result))
